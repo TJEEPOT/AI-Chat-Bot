2e8b78a4492d087b575ee9332c451a28d273bf6b
TJEEPOT <TJEEPOT@gmail.com>
2021-02-07 20:38:07 +0000

    Add and test all models. Add all Jupyter notebooks used for model testing

--- model/prediction_model.py
+++ model/prediction_model.py
@@ -10,14 +10,19 @@ Desc.   : Script holds multiple models to choose from
 History : 03/01/2020 - v1.0 - Create project file
           17/01/2020 - v1.1 - Added final KNN model as tested
           20/01/2020 - v1.1 - Added use_model function.
+          01/02/2020 - v1.2 - Added bayes model generator.
+          04/02/2020 - v1.3 - Added ANN model generator.
 
 """
 import sqlite3
 import os.path
 import pandas as pd
 import numpy as np
+from data import services
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import BayesianRidge
+from sklearn.neural_network import MLPClassifier
 
 __author__     = "Martin Siddons"
 __credits__    = ["Martin Siddons", "Steven Diep", "Sam Humphreys"]
@@ -30,14 +35,15 @@ def knn_model_trainer(station_from, station_to, network):
     """ Build a KNN model for delay prediction between the given stations and save to the provided Network object,
     which is copied onto the db.
 
-    :param str station_from:
-    :param str station_to:
+    :param str station_from: TPL code of the station the train is leaving from
+    :param str station_to:   TPL code of the station the train is next calling at
     :param services.Network network: Network to save the model to
     """
     # get the relevant data from the db
-    conn = __connect_to_db()
-    df = pd.read_sql_query(""" SELECT * FROM dataset WHERE tpl_from="{}" AND tpl_to="{}" """.format
-                           (station_from, station_to), conn)  # data_model
+    conn = __connect_to_dataset()
+    df = pd.read_sql_query(""" SELECT * FROM dataset WHERE tpl_from="{}" AND tpl_to="{}" AND delay_change<=5 
+                               AND delay_change>=-5 """.format(station_from, station_to), conn)  # data_model
+    conn.close()
 
     # preprocessing - splitting attributes (x) and labels (y)
     x = df.iloc[:, 2:-1].values
@@ -54,48 +60,134 @@ def knn_model_trainer(station_from, station_to, network):
     print("built model from", station_from, "to", station_to)
 
     # save model to network object and update network on db
-    # stn_to = network.get_station(station_to)
-    # model_scaler = [model, scaler]
-    # network.get_station(station_from).add_model(stn_to, model_scaler)
-    # services.store_network(network, conn)
+    conn = __connect_to_db()
+    stn_to = network.get_station(station_to)
+    model_scaler = [model, scaler]
+    network.get_station(station_from).add_model(stn_to, model_scaler)
+    services.store_network(network, conn)
     conn.close()
 
 
 def bayes_model_trainer(station_from, station_to, network):
-    pass
+    """ Build a Bayesian Ridge Regression Model from the requested journey and save to the provided Network object,
+    which is copied onto the db.
+
+    :param station_from: TPL code of the station the train is leaving from
+    :param station_to:   TPL code of the station the train is next calling at
+    :param services.Network network: Network to save the model to
+
+    :rtype: BayesianRidge
+    :return: BayesianRidge model to compute the delay between the given stations
+    """
+    # get the data
+    conn = __connect_to_dataset()
+    df = pd.read_sql_query(""" SELECT * FROM dataset WHERE tpl_from="{}" AND tpl_to="{}" """.format
+                           (station_from, station_to), conn)  # data_model
+    conn.close()
+
+    # split data into attributes and labels
+    x = df.iloc[:, 2:-1].values
+    y = df.iloc[:, 7].values
+
+    # build model
+    model = BayesianRidge()
+    model.fit(x, y)
+    # an algorithm is needed to accurately interpret the floating-point value given by the model as a categorical value.
+
+    # save model to network object and update network on db
+    conn = __connect_to_db()
+    stn_to = network.get_station(station_to)
+    model_scaler = [model, None]
+    network.get_station(station_from).add_model(stn_to, model_scaler)
+    services.store_network(network, conn)
+    conn.close()
 
 
 def ann_model_trainer(station_from, station_to, network):
-    pass
+    """ Build a Multi-layer Perceptron Artificial Neural Network to predict train delays between the given stations
+    and save to the provided Network object, which is copied onto the db.
+
+    :param station_from: TPL code of the station the train is leaving from
+    :param station_to: TPL code of the station the train is next calling at
+    :param services.Network network: Network to save the model to
+    """
+    # get the relevant data from the db
+    conn = __connect_to_dataset()
+    df = pd.read_sql_query(""" SELECT * FROM dataset WHERE tpl_from="{}" AND tpl_to="{}" AND delay_change<=5 
+                               AND delay_change>=-5 """.format(station_from, station_to), conn)  # data_model
+    conn.close()
+
+    # preprocessing - splitting attributes (x) and labels (y)
+    x = df.iloc[:, 2:-1].values
+    y = df.iloc[:, 7].values
+
+    # feature scaling
+    scaler = StandardScaler()
+    scaler.fit(x)
+    x = scaler.transform(x)
+
+    # run the model
+    model = MLPClassifier(hidden_layer_sizes=5, activation='tanh', max_iter=1000, learning_rate_init=0.003)
+    model.fit(x, y)
+    print("built model from", station_from, "to", station_to)
+
+    # save model to network object and update network on db
+    conn = __connect_to_db()
+    stn_to = network.get_station(station_to)
+    model_scaler = [model, scaler]
+    network.get_station(station_from).add_model(stn_to, model_scaler)
+    services.store_network(network, conn)
+    conn.close()
 
 
 def use_model(entry, delay, network):
-    model = KNeighborsClassifier()  # "KNeighboursClassifier", "Bayes", "ANN"
-
+    model = KNeighborsClassifier()  # "KNeighborsClassifier", "MLPClassifier"
     model_name = type(model).__name__
     s_from     = entry[0]
     s_to       = entry[1]
 
+    # pull the model and station objects from the Network object
     stn_from = network.get_station(s_from)
     stn_to   = network.get_station(s_to)
-    m_s      = stn_from.get_model(stn_to, model_name)
+    m_s      = stn_from.get_model(stn_to, model_name)  # list containing the model and the scaler
     model    = m_s[0]
     scaler   = m_s[1]
 
     entry.append(delay)
     data = np.array([entry])
-    rows = [0]
-    cols = ["tpl_from", "tpl_to", "day_of_week", "weekday", "off_peak", "hour_of_day", "delay"]
-    df = pd.DataFrame(data=data, index=rows, columns=cols)
+    df = pd.DataFrame(data=data)
 
+    # assign attributes, scale and transform
     x = df.iloc[:, 2:].values
-
-    # scale and transform
-    x = scaler.transform(x)
+    if scaler is not None:
+        x = scaler.transform(x)
 
     result = model.predict(x)
-    print("delay between", s_from, "and", s_to, "predicted to change by", result, "minutes")
-    return result[0]
+    result = result[0]  # change the array returned from the prediction into an integer value
+    while delay + result < -1:
+        result += 1  # quick fix for runaway negative time estimations which are not handled well by the models
+
+    change = "increase"
+    if result < 0:
+        change = "decrease"
+    print("delay between", s_from, "and", s_to, "predicted to", change, "by", result, "minutes, to", (delay + result))
+    return result
+
+
+def __connect_to_dataset():
+    """Opens and returns a connection to dataset database or exception if the connection failed
+
+    :return: connection to db
+    :raises  sqlite3.Error: if connection to db fails
+    """
+    conn = None
+    try:
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        db_path = os.path.join(base_dir, r"..\data\dataset.sqlite")
+        conn = sqlite3.connect(db_path)
+    except sqlite3.Error as e:
+        print(e)
+    return conn
 
 
 def __connect_to_db():
@@ -112,4 +204,3 @@ def __connect_to_db():
     except sqlite3.Error as e:
         print(e)
     return conn
-
